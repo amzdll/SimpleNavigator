ccfa5cf06d20620d3d156c83fcabd3e710f8233c
glenpoin <glenpoin@student.21-school.ru>
2024-01-08 02:25:57 +0300

    fix font

--- applications/graphical_interface/app_runner.cc
+++ applications/graphical_interface/app_runner.cc
@@ -3,14 +3,14 @@
 
 #include "include/main_window.h"
 
-
 int main(int argc, char *argv[]) {
   QApplication application(argc, argv);
 
-  QFile styleFile(":/styles/style.qss");
-  styleFile.open(QFile::ReadOnly);
-  QString style = QLatin1String(styleFile.readAll());
-  qApp->setStyleSheet(style);
+//  //Font settings
+  QFontDatabase::addApplicationFont(":/fonts/font/JetBrainsMono.ttf");
+  QFile style_file(":/styles/style.qss");
+  style_file.open(QFile::ReadOnly);
+  qApp->setStyleSheet(QLatin1String(style_file.readAll()));
   GraphicalInterface w;
   w.show();
   return application.exec();
